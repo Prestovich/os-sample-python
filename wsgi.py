57fb66bcbdcb32e1b5faf96446b2d1e114390dfb
Bobby <rickenbackermaster@gmail.com>
2020-08-25 08:32:41 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,27 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    return '<h1> Hellow World !! </h1>
+            <a href="../hum">Humbee</a>
+            <a href="../bo">Boeing</a>
+            <a href="../bob">Bob</a>'
+    
+@application.route("/hum")
+def humbe():
     return "Hello Humberto!"
+
+
+@application.route("/hum")
+def humbe():
+    return "Hello Humberto!"
+
+@application.route("/bob")
+def bob():
+    return "Hi Bob!"
+
+@application.route("/bo")
+def boeing():
+    return "Thank you boeing!"
 
 if __name__ == "__main__":
     application.run()
